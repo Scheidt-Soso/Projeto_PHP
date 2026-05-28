14316bebf4ead6142e51e22a0b275b096ab159f7
Scheidt-Soso <scheidtsalves@gmail.com>
2026-05-28 15:09:41 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require "dados.php";
+require "livros.php";
+
+while (true){
+
+echo "1-Cadastrar um livro"."\n";
+echo "2-Listar ". "\n";
+echo "3-Buscar" . "\n";
+echo "4-Editar". "\n";
+echo "5-Remover". "\n";
+echo "0-Sair". "\n";
+
+$opcao = readline("Escolha");
+}
+
+
+echo "\nMenu\n";
+ 
+
+
+?>
---
+++ livros.php
@@ -0,0 +1,10 @@
+<?php
+$livros=[
+"Titulo " => "Harry Potter e a Pedra Filosofal ",
+"Autor"=> "J.K. Rowling ",
+"Quantidade de Páginas"=> "264",
+"Status de leitura " => "Lido",
+]
+
+
+?>
